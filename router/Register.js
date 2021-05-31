6fbb137398b04eae2e24fbf4ef75ffd58feda89b
shaik chandu <shaikchandu24@gmail.com>
2021-05-31 22:10:30 +0530

    3g

--- router/Register.js
+++ router/Register.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../model/User");
+const User = require("../model/user.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 var sessions = require("express-session");
